913694a9663b9b292fdf992b591f09f186ba84b1
Mykhailo Kovalenko <Mykhailo_Kovalenko@epam.com>
2021-02-04 00:58:40 +0200

    google auth test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC42bsjCoPXdpiN1FVqxXcgTO6-ZNhy64A",
+    authDomain: "my-jira-app-302921.firebaseapp.com",
+    projectId: "my-jira-app-302921",
+    storageBucket: "my-jira-app-302921.appspot.com",
+    messagingSenderId: "1063918195901",
+    appId: "1:1063918195901:web:cdbede21d40348456c93d4"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC42bsjCoPXdpiN1FVqxXcgTO6-ZNhy64A",
+    authDomain: "my-jira-app-302921.firebaseapp.com",
+    projectId: "my-jira-app-302921",
+    storageBucket: "my-jira-app-302921.appspot.com",
+    messagingSenderId: "1063918195901",
+    appId: "1:1063918195901:web:cdbede21d40348456c93d4"
+  }
 };
 
 /*
